2068789ef54c6f81106887e172596c62e985c5ad
Eain Phyo <eaintphyo90@gmail.com>
2022-10-24 21:22:25 +0630

    JsonPlaceHolder with OpenFeign and spring cloud

--- src/main/java/com/example/ntierApp/NtierAppApplication.java
+++ src/main/java/com/example/ntierApp/NtierAppApplication.java
@@ -2,8 +2,10 @@ package com.example.ntierApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NtierAppApplication {
 
 	public static void main(String[] args) {
